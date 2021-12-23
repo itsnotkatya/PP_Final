c81b6d1760ef5024071bd3a18256758963b5272e
itsnotkatya <49903775+itsnotkatya@users.noreply.github.com>
2021-12-23 17:37:18 +1000

    initial commit

---
+++ cmake-build-debug/CMakeFiles/parallel_final.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ekaterina/CLionProjects/parallel_lessons-main/main.cpp" "/Users/ekaterina/CLionProjects/parallel_lessons-main/cmake-build-debug/CMakeFiles/parallel_final.dir/main.cpp.o"
+  "/Users/ekaterina/CLionProjects/parallel_lessons-main/src/functions.cpp" "/Users/ekaterina/CLionProjects/parallel_lessons-main/cmake-build-debug/CMakeFiles/parallel_final.dir/src/functions.cpp.o"
+  "/Users/ekaterina/CLionProjects/parallel_lessons-main/src/integrate_cpp.cpp" "/Users/ekaterina/CLionProjects/parallel_lessons-main/cmake-build-debug/CMakeFiles/parallel_final.dir/src/integrate_cpp.cpp.o"
+  "/Users/ekaterina/CLionProjects/parallel_lessons-main/src/integrate_omp.cpp" "/Users/ekaterina/CLionProjects/parallel_lessons-main/cmake-build-debug/CMakeFiles/parallel_final.dir/src/integrate_omp.cpp.o"
+  "/Users/ekaterina/CLionProjects/parallel_lessons-main/src/reduction.cpp" "/Users/ekaterina/CLionProjects/parallel_lessons-main/cmake-build-debug/CMakeFiles/parallel_final.dir/src/reduction.cpp.o"
+  "/Users/ekaterina/CLionProjects/parallel_lessons-main/src/threads_num.cpp" "/Users/ekaterina/CLionProjects/parallel_lessons-main/cmake-build-debug/CMakeFiles/parallel_final.dir/src/threads_num.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
